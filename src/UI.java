d1af71b8c08a8ad6d6445c3b4c755df7ad0c511c
GrantBGreat <grant@rbgn.ca>
2021-05-06 18:43:02 -0400

    work on UI

--- src/UI.java
+++ src/UI.java
@@ -6,19 +6,37 @@ public class UI {
     Password password;
     Scanner in = new Scanner(System.in);
 
+    int length;
+    boolean hasSymbols;
+    boolean hasCaps;
+    boolean hasNums;
+
 
     public UI() {
         password = new Password();
+        length = 0;
+        hasCaps = false;
+        hasSymbols = false;
+        hasNums = false;
     }
 
     public void start() {
+        System.out.println("Welcome to GrantsEpicPasswordGenerator™, press enter to begin.");
+        in.nextLine();
+
+        setOptions();
+    }
+
+    private void setOptions() {
         boolean isDone = false;
         while (!isDone) {
             
         }
     }
 
-    private String[] getOptions() {
-        return;
+    private String getInput(String inputMessage) {
+        System.out.println(inputMessage);
+        System.out.print("> ");
+        return in.nextLine();
     }
 }
