608eacf079bb907296284b7e6f99561f2e0ea607
GrantBGreat <grant@rbgn.ca>
2021-05-06 18:14:16 -0400

    start

---
+++ src/Generator.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Generator {
+    public static void main(String[] args) {
+        UI ui = new UI();
+        ui.start();;
+    }
+}
---
+++ src/UI.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class UI {
+    String password;
+    Scanner in = new Scanner(System.in);
+
+
+    public UI() {
+        password = "";
+    }
+
+    public void start() {
+        
+    }
+
+    public String generate(int length) {
+        return "";
+    }
+}
